02c24a6469019690711edaf64fe7cbf87f09ad89
IAN <ooliskoo@naver.com>
2022-12-05 17:50:21 +0900

    no message

---
+++ src/main/java/com/gokkan/gokkan/GokkanApplication.java
@@ -0,0 +1,13 @@
+package com.gokkan.gokkan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokkanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokkan/gokkan/GokkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokkan.gokkan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
