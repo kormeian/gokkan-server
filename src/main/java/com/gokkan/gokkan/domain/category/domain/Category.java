44ece8cce76c26ddc7f4480f85ef8b99b1d59b99
roomdoor <roomdoor.lee@gmail.com>
2022-12-08 10:54:42 +0900

    Fest : category entity, repo, Dto

---
+++ src/main/java/com/gokkan/gokkan/domain/category/domain/Category.java
@@ -0,0 +1,53 @@
+package com.gokkan.gokkan.domain.category.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+public class Category {
+
+	@Id
+	@Column(name = "category_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 40)
+	private String name;
+
+	private int level;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@ToString.Exclude
+	@JoinColumn(name = "par_category_id")
+	private Category parent;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "parent", cascade = CascadeType.ALL)
+	private List<Category> children = new ArrayList<>();
+
+	public static void addRelation(Category parent, Category child) {
+		parent.children.add(child);
+		child.setParent(parent);
+	}
+}
