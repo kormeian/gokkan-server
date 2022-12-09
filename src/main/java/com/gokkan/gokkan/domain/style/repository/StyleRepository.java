77b6437286be29137c69ed0a96410bfd858d1e47
roomdoor <roomdoor.lee@gmail.com>
2022-12-09 15:41:34 +0900

    Feat : style API repository

--- src/main/java/com/gokkan/gokkan/domain/style/repository/StyleRepository.java
+++ src/main/java/com/gokkan/gokkan/domain/style/repository/StyleRepository.java
@@ -1,10 +1,12 @@
 package com.gokkan.gokkan.domain.style.repository;
 
 import com.gokkan.gokkan.domain.style.domain.Style;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StyleRepository extends JpaRepository<Style, Long> {
 
+	Optional<Style> findByName(String name);
 }
