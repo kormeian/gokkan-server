cdb3a7616a3a2401d154c4f0685518c730e5e23b
roomdoor <roomdoor.lee@gmail.com>
2022-12-12 23:51:35 +0900

    Feat : ImageItem, ImageCheck ManyToOne Item 추가

--- src/main/java/com/gokkan/gokkan/domain/image/domain/ImageCheck.java
+++ src/main/java/com/gokkan/gokkan/domain/image/domain/ImageCheck.java
@@ -1,10 +1,14 @@
 package com.gokkan.gokkan.domain.image.domain;
 
+import com.gokkan.gokkan.domain.item.domain.Item;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,8 +32,8 @@ public class ImageCheck {
 
 	private String url;
 
-	//TODO Item 생성 후
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name = "item_id")
-//	private Item item;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	@ToString.Exclude
+	private Item item;
 }
