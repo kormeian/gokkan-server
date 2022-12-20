8345cc6e469f315b5e62557cc81968dfb7b4b45b
IAN <ooliskoo@naver.com>
2022-12-20 15:02:19 +0900

    Fix : 테스트 빌더 삭제

--- src/main/java/com/gokkan/gokkan/domain/style/domain/ExpertStyle.java
+++ src/main/java/com/gokkan/gokkan/domain/style/domain/ExpertStyle.java
@@ -1,7 +1,6 @@
 package com.gokkan.gokkan.domain.style.domain;
 
 import com.gokkan.gokkan.domain.expertInfo.domain.ExpertInfo;
-import com.gokkan.gokkan.domain.item.domain.Item;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,14 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -41,5 +36,9 @@ public class ExpertStyle {
 	public ExpertStyle(ExpertInfo expertInfo, Style style) {
 		this.expertInfo = expertInfo;
 		this.style = style;
+	}
+
+	public String getStyleName() {
+		return style.getName();
 	}
 }
