b55b4f2634378ab3a4071f30270e741c21fb77e8
IAN <ooliskoo@naver.com>
2022-12-20 15:03:18 +0900

    Fix : 레포지토리 수정

--- src/main/java/com/gokkan/gokkan/domain/style/repository/ExpertStyleRepository.java
+++ src/main/java/com/gokkan/gokkan/domain/style/repository/ExpertStyleRepository.java
@@ -3,11 +3,15 @@ package com.gokkan.gokkan.domain.style.repository;
 import com.gokkan.gokkan.domain.expertInfo.domain.ExpertInfo;
 import com.gokkan.gokkan.domain.style.domain.ExpertStyle;
 import com.gokkan.gokkan.domain.style.domain.Style;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExpertStyleRepository extends JpaRepository<ExpertStyle, Long> {
 
-	void deleteByExpertInfoAndStyle(ExpertInfo expertInfo, Style style);
+	List<ExpertStyle> findAllByExpertInfo(ExpertInfo expertInfo);
+
+	Optional<ExpertStyle> findByExpertInfoAndStyle(ExpertInfo expertInfo, Style style);
 }
