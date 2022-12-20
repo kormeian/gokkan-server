8e56f0ef0794b187fa97eb969909e0e0a7432d71
IAN <ooliskoo@naver.com>
2022-12-20 15:03:44 +0900

    Fix : 트랜잭션 추가, 조회 추가, 삭제 기능 수정

--- src/main/java/com/gokkan/gokkan/domain/style/service/ExpertStyleService.java
+++ src/main/java/com/gokkan/gokkan/domain/style/service/ExpertStyleService.java
@@ -9,8 +9,11 @@ import com.gokkan.gokkan.domain.style.exception.StyleErrorCode;
 import com.gokkan.gokkan.domain.style.repository.ExpertStyleRepository;
 import com.gokkan.gokkan.domain.style.repository.StyleRepository;
 import com.gokkan.gokkan.global.exception.exception.RestApiException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +23,11 @@ public class ExpertStyleService {
 	private final ExpertInfoRepository expertInfoRepository;
 	private final StyleRepository styleRepository;
 
+	@Transactional
 	public void createStyleByStyleId(Long expertInfoId, Long styleId) {
-		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId).orElseThrow(() -> new RestApiException(
-			ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
+		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId)
+			.orElseThrow(() -> new RestApiException(
+				ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
 		Style style = styleRepository.findById(styleId).orElseThrow(() -> new RestApiException(
 			StyleErrorCode.NOT_FOUND_STYLE));
 		expertStyleRepository.save(ExpertStyle.builder()
@@ -31,9 +36,11 @@ public class ExpertStyleService {
 			.build());
 	}
 
+	@Transactional
 	public void createStyleByStyleName(Long expertInfoId, String styleName) {
-		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId).orElseThrow(() -> new RestApiException(
-			ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
+		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId)
+			.orElseThrow(() -> new RestApiException(
+				ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
 		Style style = styleRepository.findByName(styleName).orElseThrow(() -> new RestApiException(
 			StyleErrorCode.NOT_FOUND_STYLE));
 		expertStyleRepository.save(ExpertStyle.builder()
@@ -42,17 +49,38 @@ public class ExpertStyleService {
 			.build());
 	}
 
+	@Transactional
 	public void deleteExpertStyle(Long expertStyleId) {
-		ExpertStyle expertStyle = expertStyleRepository.findById(expertStyleId).orElseThrow(() -> new RestApiException(
-			StyleErrorCode.NOT_FOUND_STYLE));
+		ExpertStyle expertStyle = expertStyleRepository.findById(expertStyleId)
+			.orElseThrow(() -> new RestApiException(
+				StyleErrorCode.NOT_FOUND_STYLE));
 		expertStyleRepository.delete(expertStyle);
 	}
 
+	@Transactional
 	public void deleteStyleByStyleName(Long expertInfoId, String styleName) {
-		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId).orElseThrow(() -> new RestApiException(
-			ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
+		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId)
+			.orElseThrow(() -> new RestApiException(
+				ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
 		Style style = styleRepository.findByName(styleName).orElseThrow(() -> new RestApiException(
 			StyleErrorCode.NOT_FOUND_STYLE));
-		expertStyleRepository.deleteByExpertInfoAndStyle(expertInfo, style);
+		ExpertStyle expertStyle = expertStyleRepository.findByExpertInfoAndStyle(expertInfo, style)
+			.orElseThrow(() -> new RestApiException(
+				StyleErrorCode.NOT_FOUND_STYLE));
+		expertStyleRepository.delete(expertStyle);
+	}
+
+	@Transactional(readOnly = true)
+	public List<String> getExpertStyles(Long expertInfoId) {
+		ExpertInfo expertInfo = expertInfoRepository.findById(expertInfoId)
+			.orElseThrow(() -> new RestApiException(
+				ExpertInfoErrorCode.EXPERT_INFO_NOT_FOUND));
+		List<String> allByExpertInfo = expertStyleRepository.findAllByExpertInfo(expertInfo)
+			.stream().map(
+				ExpertStyle::getStyleName).collect(Collectors.toList());
+		if (allByExpertInfo.isEmpty()) {
+			throw new RestApiException(StyleErrorCode.NOT_FOUND_STYLE);
+		}
+		return allByExpertInfo;
 	}
 }
