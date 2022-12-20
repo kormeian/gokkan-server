f8f684ba1941dd126258c2b3085522964140a411
roomdoor <roomdoor.lee@gmail.com>
2022-12-20 15:29:49 +0900

    Fix : ItemController member add and fix create, update
    create, update 모두 서비스단으로 변경

--- src/main/java/com/gokkan/gokkan/domain/item/controller/ItemController.java
+++ src/main/java/com/gokkan/gokkan/domain/item/controller/ItemController.java
@@ -1,22 +1,14 @@
 package com.gokkan.gokkan.domain.item.controller;
 
-import com.gokkan.gokkan.domain.category.domain.Category;
-import com.gokkan.gokkan.domain.category.service.CategoryService;
-import com.gokkan.gokkan.domain.image.domain.ImageCheck;
-import com.gokkan.gokkan.domain.image.domain.ImageItem;
-import com.gokkan.gokkan.domain.image.service.AwsS3Service;
-import com.gokkan.gokkan.domain.image.service.ImageCheckService;
-import com.gokkan.gokkan.domain.image.service.ImageItemService;
 import com.gokkan.gokkan.domain.item.dto.ItemDto;
 import com.gokkan.gokkan.domain.item.service.ItemService;
-import com.gokkan.gokkan.domain.style.domain.StyleItem;
-import com.gokkan.gokkan.domain.style.service.StyleItemService;
+import com.gokkan.gokkan.domain.member.domain.Member;
+import com.gokkan.gokkan.global.security.oauth.token.CurrentMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -39,11 +31,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class ItemController {
 
 	private final ItemService itemService;
-	private final CategoryService categoryService;
-	private final StyleItemService styleItemService;
-	private final ImageItemService imageItemService;
-	private final ImageCheckService imageCheckService;
-	private final AwsS3Service awsS3Service;
 
 	@Operation(summary = "상품 생성", description = "상품 생성, Amazon S3에 파일 업로드, 업로드 된 이미지 url 상품에 저장")
 	@ApiResponse(responseCode = "201", description = "생성된 상품 반환", content = @Content(schema = @Schema(implementation = ItemDto.Response.class)))
@@ -55,17 +42,12 @@ public class ItemController {
 		@Parameter(description = "상품 이미지 파일 (여러 파일 업로드 가능)", required = true)
 		@RequestPart List<MultipartFile> imageItemFiles,
 		@Parameter(description = "검수 이미지 파일 (여러 파일 업로드 가능)", required = true)
-		@RequestPart List<MultipartFile> imageCheckFiles) {
+		@RequestPart List<MultipartFile> imageCheckFiles,
+		@Parameter(hidden = true)
+		@CurrentMember Member member) {
 
-
-		checkBeforeSaveInS3(imageItemFiles, imageCheckFiles);
-    
-		Category category = categoryService.getCategory(request.getCategory());
-		List<StyleItem> styleItems = styleItemService.create(request.getStyles());
-		List<ImageItem> imageItems = imageItemService.save(awsS3Service.save(imageItemFiles));
-		List<ImageCheck> imageChecks = imageCheckService.save(awsS3Service.save(imageCheckFiles));
 		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(itemService.create(request, imageItems, imageChecks, category, styleItems));
+			.body(itemService.create(request, imageItemFiles, imageCheckFiles, member));
 	}
 
 	@Operation(summary = "상품 조회", description = "itemId에 해당하는 상품 조회")
@@ -80,8 +62,12 @@ public class ItemController {
 	@Operation(summary = "상품 삭제", description = "itemId에 해당하는 상품 삭제")
 	@ApiResponse(responseCode = "200", description = "itemId에 해당하는 상품 삭제")
 	@DeleteMapping
-	public ResponseEntity<?> delete(@RequestParam Long itemId) {
-		return ResponseEntity.ok(itemService.delete(itemId));
+	public ResponseEntity<?> delete(
+		@Parameter(description = "상품 아이디", required = true)
+		@RequestParam Long itemId,
+		@Parameter(hidden = true)
+		@CurrentMember Member member) {
+		return ResponseEntity.ok(itemService.delete(itemId, member));
 	}
 
 	@Operation(summary = "상품 수정", description = "상품 수정, Amazon S3에 파일 업로드, 수정된 이미지 url 상품에 저장")
@@ -94,21 +80,11 @@ public class ItemController {
 		@Parameter(description = "상품 이미지 파일 (여러 파일 업로드 가능)", required = true)
 		@RequestPart List<MultipartFile> imageItemFiles,
 		@Parameter(description = "검수 이미지 파일 (여러 파일 업로드 가능)", required = true)
-		@RequestPart List<MultipartFile> imageCheckFiles) {
-
-		checkBeforeSaveInS3(imageItemFiles, imageCheckFiles);
-		Category category = categoryService.getCategory(request.getCategory());
-		List<StyleItem> styleItems = styleItemService.create(request.getStyles());
-		List<ImageItem> imageItems = imageItemService.save(awsS3Service.save(imageItemFiles));
-		List<ImageCheck> imageChecks = imageCheckService.save(awsS3Service.save(imageCheckFiles));
+		@RequestPart List<MultipartFile> imageCheckFiles,
+		@Parameter(hidden = true)
+		@CurrentMember Member member) {
 
 		return ResponseEntity.ok(
-			itemService.update(request, imageItems, imageChecks, category, styleItems));
-	}
-
-	private void checkBeforeSaveInS3(List<MultipartFile> imageItemFiles,
-		List<MultipartFile> imageCheckFiles) {
-		awsS3Service.check(imageItemFiles);
-		awsS3Service.check(imageCheckFiles);
+			itemService.update(request, imageItemFiles, imageCheckFiles, member));
 	}
 }
