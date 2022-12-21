fd532b918112d29fba52240ce5927c614c875cd7
IAN <ooliskoo@naver.com>
2022-12-21 18:19:28 +0900

    병합해결

--- src/main/java/com/gokkan/gokkan/domain/member/repository/MemberRepository.java
+++ src/main/java/com/gokkan/gokkan/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.gokkan.gokkan.domain.member.repository;
 
 import com.gokkan.gokkan.domain.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Member findByUserId(String userId);
+	Optional<Member> findByNickName(String nickName);
 
 	boolean existsByNickName(String nickName);
 }
