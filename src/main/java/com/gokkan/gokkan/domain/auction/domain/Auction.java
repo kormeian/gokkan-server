f290f55a61906c58d98a5b8be3237d8328e5f142
IAN <ooliskoo@naver.com>
2022-12-21 21:32:10 +0900

    Feat : 경매 엔티티

---
+++ src/main/java/com/gokkan/gokkan/domain/auction/domain/Auction.java
@@ -0,0 +1,52 @@
+package com.gokkan.gokkan.domain.auction.domain;
+
+import com.gokkan.gokkan.domain.auction.domain.type.AuctionStatus;
+import com.gokkan.gokkan.domain.expertComment.domain.ExpertComment;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Auction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "auction_id")
+	private Long id;
+
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+
+	private Long startPrice;
+	private Long currentPrice;
+
+	@Enumerated(value = EnumType.STRING)
+	private AuctionStatus auctionStatus;
+
+	@JoinColumn(name = "expert_comment_id")
+	@OneToOne
+	private ExpertComment expertComment;
+
+	@Builder
+	public Auction(LocalDateTime startDateTime, LocalDateTime endDateTime, Long startPrice, Long currentPrice, AuctionStatus auctionStatus, ExpertComment expertComment) {
+		this.startDateTime = startDateTime;
+		this.endDateTime = endDateTime;
+		this.startPrice = startPrice;
+		this.currentPrice = currentPrice;
+		this.auctionStatus = auctionStatus;
+		this.expertComment = expertComment;
+	}
+}
