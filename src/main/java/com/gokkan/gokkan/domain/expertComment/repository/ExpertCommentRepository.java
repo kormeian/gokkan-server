3356374124169d9f7502eab9ef1d3e20fbde74e6
IAN <ooliskoo@naver.com>
2022-12-22 23:38:19 +0900

    Fix : 전문가 의견 레포지토리 메소드 추가

--- src/main/java/com/gokkan/gokkan/domain/expertComment/repository/ExpertCommentRepository.java
+++ src/main/java/com/gokkan/gokkan/domain/expertComment/repository/ExpertCommentRepository.java
@@ -2,7 +2,9 @@ package com.gokkan.gokkan.domain.expertComment.repository;
 
 import com.gokkan.gokkan.domain.expertComment.domain.ExpertComment;
 import com.gokkan.gokkan.domain.expertInfo.domain.ExpertInfo;
+import com.gokkan.gokkan.domain.item.domain.Item;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,8 @@ public interface ExpertCommentRepository extends JpaRepository<ExpertComment, Lo
 
 
 	List<ExpertComment> findAllByExpertInfo(ExpertInfo expertInfo);
+
+	boolean existsByExpertInfoAndItem(ExpertInfo expertInfo, Item item);
+
+	ExpertComment findByExpertInfoAndItem(ExpertInfo expertInfo, Item item);
 }
