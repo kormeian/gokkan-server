15f5767e745d95af391ecfa44f3fa4077d52b0a5
IAN <ooliskoo@naver.com>
2022-12-22 23:38:08 +0900

    Fix : 전문가 의견 에러코드 추가

--- src/main/java/com/gokkan/gokkan/domain/expertComment/exception/ExpertCommentErrorCode.java
+++ src/main/java/com/gokkan/gokkan/domain/expertComment/exception/ExpertCommentErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ExpertCommentErrorCode implements ErrorCode {
 
 	ITEM_STATE_NOT_ASSESSING(HttpStatus.BAD_REQUEST, "아이템 상태가 감정중이 아닙니다."),
-	NOT_FOUND_STATE(HttpStatus.BAD_REQUEST, "상태가 존재하지 않습니다.");
+	NOT_FOUND_STATE(HttpStatus.BAD_REQUEST, "상태가 존재하지 않습니다."),
+	NOT_FOUND_EXPERT_COMMENT(HttpStatus.NOT_FOUND, "전문가 코멘트가 존재하지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
