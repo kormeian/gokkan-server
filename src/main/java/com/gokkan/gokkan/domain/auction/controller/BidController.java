15bcfd13c077773d028b6d4f6816d399eaf4073f
IAN <ooliskoo@naver.com>
2022-12-23 15:51:08 +0900

    Feat : 테스트용 웹소켓 구현

---
+++ src/main/java/com/gokkan/gokkan/domain/auction/controller/BidController.java
@@ -0,0 +1,10 @@
+package com.gokkan.gokkan.domain.auction.controller;
+
+//@Controller
+//public class BidController {
+//	@MessageMapping("/bid")
+//	@SendTo("/topic/bids")
+//	public String bid(String bid) {
+//		return bid;
+//	}
+//}
