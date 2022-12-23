cb0d0c88727ffa8d445a28e1b30e40370bda11c7
roomdoor <roomdoor.lee@gmail.com>
2022-12-23 16:08:51 +0900

    Test : imageItemService

--- src/main/java/com/gokkan/gokkan/domain/image/service/ImageItemService.java
+++ src/main/java/com/gokkan/gokkan/domain/image/service/ImageItemService.java
@@ -1,6 +1,7 @@
 package com.gokkan.gokkan.domain.image.service;
 
 import com.gokkan.gokkan.domain.image.domain.ImageItem;
+import com.gokkan.gokkan.domain.image.dto.ImageDto.UpdateRequest;
 import com.gokkan.gokkan.domain.image.exception.ImageErrorCode;
 import com.gokkan.gokkan.domain.image.repository.ImageItemRepository;
 import com.gokkan.gokkan.global.exception.exception.RestApiException;
@@ -19,15 +20,8 @@ public class ImageItemService {
 	private final AwsS3Service awsS3Service;
 
 	public List<ImageItem> create(List<String> urls) {
-		if (urls.size() == 0) {
-			throw new RestApiException(ImageErrorCode.EMPTY_URL);
-		}
-
 		List<ImageItem> imageItems = new ArrayList<>();
 		for (String url : urls) {
-			if (url == null || url.length() == 0) {
-				throw new RestApiException(ImageErrorCode.INVALID_FORMAT_URL);
-			}
 			imageItems.add(
 				ImageItem.builder()
 					.url(url)
@@ -55,8 +49,49 @@ public class ImageItemService {
 		return true;
 	}
 
+	public void deleteAllImageItems(List<ImageItem> saved) {
+		for (ImageItem imageItem : saved) {
+			delete(imageItem);
+		}
+	}
+
 	private ImageItem getImageItem(Long imageItemId) {
 		return imageItemRepository.findById(imageItemId)
 			.orElseThrow(() -> new RestApiException(ImageErrorCode.NOT_FOUND_IMAGE_ITEM));
+	}
+
+	public List<ImageItem> checkImageItemDeleted(List<UpdateRequest> urls, List<ImageItem> saved) {
+		List<ImageItem> imageItems = new ArrayList<>();
+		boolean[] deleted = new boolean[saved.size()];
+		int deletedCount = saved.size() - urls.size();
+
+		if (deletedCount == 0) {
+			return saved;
+		} else if (deletedCount == saved.size()) {
+			deleteAllImageItems(saved);
+			return new ArrayList<>();
+		} else {
+			int nextStartIndex = 0;
+
+			for (ImageItem imageItem : saved) {
+				for (int j = nextStartIndex; j < urls.size(); j++) {
+					if (imageItem.getId().equals(urls.get(j).getImageId())) {
+						deleted[j] = true;
+						nextStartIndex = j;
+						break;
+					}
+
+				}
+			}
+
+			for (int i = 0; i < deleted.length; i++) {
+				if (deleted[i]) {
+					imageItems.add(saved.get(i));
+				} else {
+					delete(saved.get(i));
+				}
+			}
+			return imageItems;
+		}
 	}
 }
