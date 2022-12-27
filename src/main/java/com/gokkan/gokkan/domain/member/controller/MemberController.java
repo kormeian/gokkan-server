67824c0c9183b43fc69a74aad140b12b48c2f02f
IAN <ooliskoo@naver.com>
2022-12-27 17:53:52 +0900

    Feat : 로그아웃 기능 추가

--- src/main/java/com/gokkan/gokkan/domain/member/controller/MemberController.java
+++ src/main/java/com/gokkan/gokkan/domain/member/controller/MemberController.java
@@ -15,8 +15,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -96,6 +98,14 @@ public class MemberController {
 		@Parameter(description = "닉네임")
 		@RequestParam String nickName) {
 		return ResponseEntity.ok(memberService.checkDuplicateNickName(nickName));
+	}
+
+	@PostMapping("/logout")
+	@Operation(summary = "로그아웃", description = "로그아웃")
+	public ResponseEntity<Void> logout(
+		@Parameter(hidden = true) @CurrentMember Member member) {
+		memberService.logout(member);
+		return ResponseEntity.ok().build();
 	}
 }
 
