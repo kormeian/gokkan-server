c7dd611f1a79f743bdd989d4e6b10332c5ee86ac
roomdoor <roomdoor.lee@gmail.com>
2022-12-27 15:11:24 +0900

    Fix : 잡다한 수정

--- src/main/java/com/gokkan/gokkan/global/querydsl/config/QueryDslConfig.java
+++ src/main/java/com/gokkan/gokkan/global/querydsl/config/QueryDslConfig.java
@@ -2,15 +2,15 @@ package com.gokkan.gokkan.global.querydsl.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import javax.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDslConfig {
 
-	private final EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Bean
 	public JPAQueryFactory jpaQueryFactory() {
