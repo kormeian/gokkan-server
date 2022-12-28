a27761b14a42db45232f374512bc4e83591b8070
김이안 <kormeian>
2022-12-28 18:01:47 +0900

    Feat : 웹소켓 토큰인증 핸들러 추가

---
+++ src/main/java/com/gokkan/gokkan/global/webSocket/interceptor/StompChannelInterceptor.java
@@ -0,0 +1,64 @@
+package com.gokkan.gokkan.global.webSocket.interceptor;
+
+import com.gokkan.gokkan.domain.member.domain.Member;
+import com.gokkan.gokkan.domain.member.exception.AuthErrorCode;
+import com.gokkan.gokkan.domain.member.repository.MemberRepository;
+import com.gokkan.gokkan.global.exception.exception.RestApiException;
+import com.gokkan.gokkan.global.security.oauth.token.AuthToken;
+import com.gokkan.gokkan.global.security.oauth.token.AuthTokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class StompChannelInterceptor implements ChannelInterceptor {
+
+	private static final String BEARER_PREFIX = "Bearer ";
+	private final AuthTokenProvider tokenProvider;
+	private final MemberRepository memberRepository;
+
+	private Member member;
+
+	private void setMember(Member tokenMember){
+		this.member = tokenMember;
+	}
+	public Member getMember(){
+		return this.member;
+	}
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+		if(headerAccessor.getCommand() != StompCommand.SEND){
+			return message;
+		}
+
+		String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
+		if(authorizationHeader == null || authorizationHeader.equals("null")){
+			log.info("토큰 없음!");
+			return message;
+			//throw new RestApiException(AuthErrorCode.AUTHORIZATION_HEADER_NOT_FOUND);
+		}
+		String tokenStr = authorizationHeader.substring(BEARER_PREFIX.length());
+		AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+		if (token.validate()) {
+			log.info("토큰 유효 멤버 조회");
+			Member member = memberRepository.findByUserId(token.getTokenClaims().getSubject());
+			setMember(member);
+		}
+
+		return message;
+	}
+}
