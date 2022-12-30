64533a7801635e12e6ddff472237164bacd5337c
IAN <ooliskoo@naver.com>
2022-12-30 20:53:33 +0900

    Fix : 로그추가

--- src/main/java/com/gokkan/gokkan/domain/auction/controller/BidController.java
+++ src/main/java/com/gokkan/gokkan/domain/auction/controller/BidController.java
@@ -4,12 +4,14 @@ import com.gokkan.gokkan.domain.auction.service.BidService;
 import com.gokkan.gokkan.domain.member.domain.Member;
 import com.gokkan.gokkan.global.webSocket.interceptor.StompChannelInterceptor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BidController {
 
 	private final BidService bidService;
@@ -20,6 +22,7 @@ public class BidController {
 		@PathVariable Long auctionId,
 		Long price) {
 		Member member = stompChannelInterceptor.getMember();
+		log.info("멤버 아이디 : " + member.getId() + "가 입찰을 시작합니다. 경매 아이디 : " + auctionId + " 입찰 가격 : " + price);
 		bidService.bidding(member, auctionId, price);
 	}
 }
