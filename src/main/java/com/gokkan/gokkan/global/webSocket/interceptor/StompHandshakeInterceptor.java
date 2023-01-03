c438299d87847fd93ebbbd08b6adf3bcce3656ee
IAN <ooliskoo@naver.com>
2023-01-04 05:23:54 +0900

    feat : 예외처리에 필요한 sessionId 저장

--- src/main/java/com/gokkan/gokkan/global/webSocket/interceptor/StompHandshakeInterceptor.java
+++ src/main/java/com/gokkan/gokkan/global/webSocket/interceptor/StompHandshakeInterceptor.java
@@ -1,9 +1,11 @@
 package com.gokkan.gokkan.global.webSocket.interceptor;
 
 import java.util.Map;
+import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -16,6 +18,13 @@ public class StompHandshakeInterceptor implements HandshakeInterceptor {
 	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
 		WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
 		log.info("stomp handshake start");
+		if (request instanceof ServletServerHttpRequest) {
+			ServletServerHttpRequest servletRequest
+				= (ServletServerHttpRequest) request;
+			HttpSession session = servletRequest
+				.getServletRequest().getSession();
+			attributes.put("sessionId", session.getId());
+		}
 		return true;
 	}
 
