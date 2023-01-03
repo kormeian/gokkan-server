edfd945fa94bdaad953655eb81814ae016a08b38
IAN <ooliskoo@naver.com>
2023-01-04 05:21:38 +0900

    feat : 스케줄러, 결제대기에서 필요한 메소드 추가

--- src/main/java/com/gokkan/gokkan/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/gokkan/gokkan/domain/auction/repository/AuctionRepository.java
@@ -1,11 +1,16 @@
 package com.gokkan.gokkan.domain.auction.repository;
 
 import com.gokkan.gokkan.domain.auction.domain.Auction;
+import com.gokkan.gokkan.domain.auction.domain.type.AuctionStatus;
+import com.gokkan.gokkan.domain.member.domain.Member;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
+	List<Auction> findAllByEndDateTimeLessThanEqualAndAuctionStatusEquals(LocalDateTime currentDateTime, AuctionStatus auctionStatus);
 
-
+	List<Auction> findAllByAuctionStatusEqualsAndMemberEquals(AuctionStatus auctionStatus, Member member);
 }
